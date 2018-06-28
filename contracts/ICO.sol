5aad8881b84fb47c5dff19b510da340de898e230
hihiben <hihiben@gmail.com>
2018-06-28 17:38:39 +0800

    Add several function to be limited
    approve, increaseApproval, decreaseApproval should be limited
    when Paused and Timelocked

--- contracts/ICO.sol
+++ contracts/ICO.sol
@@ -480,6 +480,18 @@ contract TimelockToken is Whitelist, Timelock, StandardToken {
     function transferFrom(address _from, address _to, uint256 _value) public whenTransferrable returns (bool) {
         return super.transferFrom(_from, _to, _value);
     }
+
+    function approve(address _spender, uint256 _value) public whenTransferrable returns (bool) {
+        return super.approve(_spender, _value);
+    }
+
+    function increaseApproval(address _spender, uint _addedValue) public whenTransferrable returns (bool) {
+        return super.increaseApproval(_spender, _addedValue);
+    }
+
+    function decreaseApproval(address _spender, uint _subtractedValue) public whenTransferrable returns (bool) {
+        return super.decreaseApproval(_spender, _subtractedValue);
+    }
 }
 
 
@@ -495,6 +507,18 @@ contract PausableToken is Pausable, StandardToken {
 
     function transferFrom(address _from, address _to, uint256 _value) public whenNotPaused returns (bool) {
         return super.transferFrom(_from, _to, _value);
+    }
+
+    function approve(address _spender, uint256 _value) public whenNotPaused returns (bool) {
+        return super.approve(_spender, _value);
+    }
+
+    function increaseApproval(address _spender, uint _addedValue) public whenNotPaused returns (bool) {
+        return super.increaseApproval(_spender, _addedValue);
+    }
+
+    function decreaseApproval(address _spender, uint _subtractedValue) public whenNotPaused returns (bool) {
+        return super.decreaseApproval(_spender, _subtractedValue);
     }
 }
 
