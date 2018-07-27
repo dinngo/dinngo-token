d75cd751d26af3254ee53dd16b586222df312adf
hihiben <hihiben@gmail.com>
2018-07-27 19:04:44 +0800

    truffle migrate

--- truffle.js
+++ truffle.js
@@ -1,4 +1,21 @@
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
+    networks: {
+        ropsten: {
+            network_id: 3,
+            host: "localhost",
+            port: 8545,
+            gas: 4700000,
+            gasPrice: 22000000000
+        }
+    },
+    rpc: {
+        host: "localhost",
+        port: 8080
+    },
+    solc: {
+        optimizer: {
+            enabled: true,
+            runs: 200
+        }
+    }
 };
