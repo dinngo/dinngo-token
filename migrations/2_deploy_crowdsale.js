3aad2737ad9c5c6b6909c74346d3df6532c0ef59
hihiben <hihiben@gmail.com>
2018-07-30 15:26:18 +0800

    deploy token and crowdsale in order

--- migrations/2_deploy_crowdsale.js
+++ migrations/2_deploy_crowdsale.js
@@ -1,8 +1,15 @@
+var DinngoToken = artifacts.require("./DinngoToken.sol");
 var DinngoCrowdsale = artifacts.require("./DinngoCrowdsale.sol");
 
 module.exports = function(deployer) {
-  const rate = 2125;
-  const tokenWallet = "0xe46c697f989c0aeec08bbdb765059dce8b065e03";
-  const fundsWallet = "0xad6368c6659e3ac2e15623566b767022708efd11";
-  deployer.deploy(DinngoCrowdsale, rate, tokenWallet, fundsWallet, {gas: 4700000});
+    const rate = 2125;
+    const tokenWallet = "0xe46c697f989c0aeec08bbdb765059dce8b065e03";
+    const fundsWallet = "0xad6368c6659e3ac2e15623566b767022708efd11";
+    deployer.deploy(DinngoToken, tokenWallet).then(() => {
+        return DinngoToken.deployed();
+    }).then(function() {
+        deployer.deploy(DinngoCrowdsale, DinngoToken.address, rate, tokenWallet, fundsWallet).then(() => {
+            return DinngoCrowdsale.deployed();
+        });
+    });
 };
