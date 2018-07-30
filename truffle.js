589799c59a5ed92b474a0184bcd2ed90747f7d2c
hihiben <hihiben@gmail.com>
2018-07-30 15:44:13 +0800

    add network development

--- truffle.js
+++ truffle.js
@@ -1,5 +1,10 @@
 module.exports = {
     networks: {
+        development: {
+            host: "127.0.0.1",
+            port: 8545,
+            network_id: "*"
+        },
         ropsten: {
             network_id: 3,
             host: "localhost",
