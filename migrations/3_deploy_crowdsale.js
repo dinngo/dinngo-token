44c401fcc666b532c299dc43e8957e2b0d147b3b
hihiben <hihiben@gmail.com>
2018-08-01 16:59:29 +0800

    add the crowdsale contract address into the token whitelist

--- migrations/3_deploy_crowdsale.js
+++ migrations/3_deploy_crowdsale.js
@@ -5,5 +5,9 @@ module.exports = function(deployer) {
     const rate = 2125;
     const tokenWallet = "0xe46c697f989c0aeec08bbdb765059dce8b065e03";
     const fundsWallet = "0xad6368c6659e3ac2e15623566b767022708efd11";
-    deployer.deploy(DinngoCrowdsale, DinngoToken.address, rate, tokenWallet, fundsWallet);
+    deployer.deploy(DinngoCrowdsale, DinngoToken.address, rate, tokenWallet, fundsWallet).then(function() {
+        DinngoToken.at(DinngoToken.address).then(inst => {
+            inst.addToWhitelist(DinngoCrowdsale.address);
+        });
+    });
 };
