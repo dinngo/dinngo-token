496f8aa9fce66431db35bc5f2f8e2f755b3d075d
hihiben <hihiben@gmail.com>
2018-08-06 16:42:46 +0800

    modify the migration file

--- migrations/3_deploy_crowdsale.js
+++ migrations/3_deploy_crowdsale.js
@@ -3,8 +3,8 @@ let DinngoToken = artifacts.require("./DinngoToken.sol");
 
 module.exports = function(deployer) {
     const rate = 2125;
-    const tokenWallet = "0xe46c697f989c0aeec08bbdb765059dce8b065e03";
-    const fundsWallet = "0xad6368c6659e3ac2e15623566b767022708efd11";
+    const tokenWallet = /* token wallet address */;
+    const fundsWallet = /* funds wallet address */;
     deployer.deploy(DinngoCrowdsale, DinngoToken.address, rate, tokenWallet, fundsWallet).then(function() {
         DinngoToken.at(DinngoToken.address).then(inst => {
             inst.addToWhitelist(DinngoCrowdsale.address);
--- migrations/5_deploy_company.js
+++ migrations/5_deploy_company.js
@@ -2,6 +2,6 @@ const DinngoCompany = artifacts.require("./DinngoCompany.sol");
 let DinngoToken = artifacts.require("./DinngoToken.sol");
 
 module.exports = function(deployer) {
-    const companyWallet = "0xe46c697f989c0aeec08bbdb765059dce8b065e03";
+    const companyWallet = /* company wallet address */;
     deployer.deploy(DinngoCompany, DinngoToken.address, companyWallet);
 }
